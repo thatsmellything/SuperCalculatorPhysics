d8c224908c2686ebbd56492fc424a678c0c127c4
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-10 08:16:10 -0600

    Created 4 types of functions

--- src/controller/PhysicsController.java
+++ src/controller/PhysicsController.java
@@ -1,6 +1,59 @@
 package controller;
 
+import java.util.Scanner;
+
 public class PhysicsController
 {
 
+	public void start()
+	{
+		System.out.print("This is the super calculator Physics Version!");
+		System.out.println("Please select what you want to find by typing it exactly");
+		System.out.println("Weight Conversion");
+		System.out.println("Potential Energy");
+		System.out.println("Kinetic Energy");
+		System.out.println("Net Energy");
+		Scanner inputScanner = new Scanner(System.in);
+		String answer = inputScanner.nextLine();
+		
+		if (answer.equals("Weight Conversion"))
+		{
+			weightConversion();
+		}
+		if (answer.equals("Potential Energy"))
+		{
+			pEnergy();
+		}
+		
+		if (answer.equals("Kinetic Energy"))
+		{
+			kEnergy();
+		}
+		
+		if (answer.equals("Net Energy"))
+		{
+			nEnergy();
+		}
+		
+	}
+	
+	public void weightConversion()
+	{
+		
+	}
+
+	public void pEnergy()
+	{
+		
+	}
+	
+	public void kEnergy()
+	{
+		
+	}
+	
+	public void nEnergy()
+	{
+		
+	}
 }
